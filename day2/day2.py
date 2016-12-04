6dddb3dca9d47fd91d84291f44559176aa2189e1
Selynna Sun <ssun098@gmail.com>
2016-12-04 02:34:49 -0800

    Day 2b

--- day2/day2.py
+++ day2/day2.py
@@ -1,33 +1,35 @@
 fileContent = open("day2-input.txt").read()
 instrList = fileContent.rstrip().split('\n')
 
-print instrList
-
 keypad = []
-keypad.append([1, 2, 3])
-keypad.append([4, 5, 6])
-keypad.append([7, 8, 9])
+# keypad.append([1, 2, 3])
+# keypad.append([4, 5, 6])
+# keypad.append([7, 8, 9])
+keypad.append([0, 0, 1, 0, 0])
+keypad.append([0, 2, 3, 4, 0])
+keypad.append([5, 6, 7, 8, 9])
+keypad.append([0, "A", "B", "C", 0])
+keypad.append([0, 0, "D", 0 , 0])
 
-print keypad
 
 code = []
 
-startingRowPosition = 1
-startingColPosition = 1
+startingRowPosition = 2
+startingColPosition = 0
 
 for i in range(len(instrList)):
     for j in range(len(instrList[i])):
         if instrList[i][j] == "L":
-            if startingColPosition - 1 != -1:
+            if startingColPosition - 1 >= 0 and startingColPosition - 1 < 5 and keypad[startingRowPosition][startingColPosition - 1] != 0:
                 startingColPosition = startingColPosition - 1
         elif instrList[i][j] == "U":
-            if startingRowPosition - 1 != -1:
+            if startingRowPosition - 1 >= 0 and startingRowPosition - 1 < 5 and keypad[startingRowPosition - 1][startingColPosition] != 0:
                 startingRowPosition = startingRowPosition - 1
         elif instrList[i][j] == "R":
-            if startingColPosition + 1 != 3:
+            if startingColPosition + 1 >= 0 and startingColPosition + 1 < 5 and keypad[startingRowPosition][startingColPosition + 1] != 0:
                 startingColPosition = startingColPosition + 1
         else:
-            if startingRowPosition + 1 != 3:
+            if startingRowPosition + 1 >= 0 and startingRowPosition + 1 < 5 and keypad[startingRowPosition + 1][startingColPosition] != 0:
                 startingRowPosition = startingRowPosition + 1
 
     code.append(keypad[startingRowPosition][startingColPosition])
