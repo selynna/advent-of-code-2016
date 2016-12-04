64a60e5993ecbcd92e6075de62fb4a41f7eb371c
Selynna Sun <ssun098@gmail.com>
2016-12-04 00:30:43 -0800

    Add day 1

---
+++ day1.py
@@ -0,0 +1,77 @@
+# import file
+
+fileInput = open("day1-input.txt").read()
+directionList = fileInput.split(", ")
+
+eastBlocks = 0
+westBlocks = 0
+northBlocks = 0
+southBlocks = 0
+directionIndex = 0
+
+grid = []
+row = []
+col = []
+
+for a in range(0, 300):
+    for b in range(0, 300):
+        col.append(0)
+    row.append(col)
+
+startingRowPosition = 0
+startingColumnPosition = 0
+
+for i in directionList:
+    if i[:1] == "R":
+        directionIndex += 1
+        if directionIndex == 4:
+            directionIndex = 0
+    else:
+        directionIndex -= 1
+        if directionIndex == -1:
+            directionIndex = 3
+    j = 0
+    k = 0
+    l = 0
+    m = 0
+
+    if directionIndex == 0:
+        northBlocks += (int)(i[1:])
+        for j in range((int)(i[1:])):
+            startingRowPosition -= 1
+            pos = str(startingRowPosition) + " " + str(startingColumnPosition)
+            if pos in grid:
+                print abs(startingRowPosition) + abs(startingColumnPosition)
+            grid.append(pos)
+
+    elif directionIndex == 1:
+        eastBlocks += (int)(i[1:])
+        for k in range((int)(i[1:])):
+            startingColumnPosition += 1
+            pos = str(startingRowPosition) + " " + str(startingColumnPosition)
+            if pos in grid:
+                print abs(startingRowPosition) + abs(startingColumnPosition)
+            grid.append(pos)
+    elif directionIndex == 2:
+        southBlocks += (int)(i[1:])
+        for l in range((int)(i[1:])):
+            startingRowPosition += 1
+            pos = str(startingRowPosition) + " " + str(startingColumnPosition)
+            if pos in grid:
+                print abs(startingRowPosition) + abs(startingColumnPosition)
+            grid.append(pos)
+    else:
+        westBlocks += (int)(i[1:])
+        for m in range((int)(i[1:])):
+            startingColumnPosition -= 1
+            pos = str(startingRowPosition) + " " + str(startingColumnPosition)
+            if pos in grid:
+                print abs(startingRowPosition) + abs(startingColumnPosition)
+            grid.append(pos)
+
+
+horizontalBlocks = eastBlocks - westBlocks
+verticalBlocks = northBlocks - southBlocks
+
+totalBlocks = abs(horizontalBlocks) + abs(verticalBlocks)
+print totalBlocks
